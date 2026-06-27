d8ba2379a3e0f6400cb97f03987f5372d29857ec
lucie815 <lorrainedamiba4@gmail.com>
2026-06-27 12:34:53 +0000

    Ajouter les salles disponible

--- GestionnaireReservation.java
+++ GestionnaireReservation.java
@@ -3,6 +3,16 @@ import java.util.List;
 
 public class GestionnaireReservation {
      //Le squelette de base que l'equipe va faire evoluer
+     private List<Resarvation> reservations = new ArrayList<>();
+     public boolean estDisponible(Salle salle, String creneau) {
+      for (Resarvation r : reservations) {
+         if (r.getSalle().getNom().equals(salle.getNom())&& r.getCreneau().equals(creneau)) {
+            return false;
+         }
+      }
+      return true;
+     }
+     
      public static void main(string[] args){
         System.out.println();
      }
