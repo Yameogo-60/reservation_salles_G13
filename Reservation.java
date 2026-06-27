85f3ce5e244b2f66f2be1a69b2524b2b67f3e225
nassiratounombre <nassiratounombre606@gmail.com>
2026-06-27 12:58:30 +0000

    Ajout des attributs dans la classe reservation.java

--- Reservation.java
+++ Reservation.java
@@ -1,3 +1,41 @@
-public class Reservation{
-    //Le developpeur 2
+//Le developpeur 2
+public class Reservation {
+
+    private int id;
+    private String salle;
+    private String etudiant;
+    private String temps_reservation;
+
+    public Reservation(int id, String salle, String etudiant, String temps_reservation) {
+        this.id = id;
+        this.salle = salle;
+        this.etudiant = etudiant;
+        this.temps_reservation = temps_reservation;
+    }
+
+    // Getters
+    public int getId() { 
+        return id;
+         }
+
+    public String getSalle() { 
+        return salle;
+         }
+
+    public String getEtudiant() {
+         return etudiant; 
+         }
+
+    public String getTemps_reservation() { 
+        return temps_reservation; 
+        }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+               "id=" + id + "
+               , etudiant='" + etudiant + "'
+               , salle='" + salle + "' 
+               , temps_reservation='" + temps_reservation + "'}";
+    }
 }
