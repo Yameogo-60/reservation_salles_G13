fe537ab238c8dedd298a419a9447d38f26fafad0
Ouagraoua Kevin <ouagraouakevin1@gmail.com>
2026-06-28 01:06:16 +0000

    ajout des outils de base et verification des salles

--- GestionnaireReservation.java
+++ GestionnaireReservation.java
@@ -2,18 +2,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GestionnaireReservation {
-     //Le squelette de base que l'equipe va faire evoluer
-     private List<Resarvation> reservations = new ArrayList<>();
-     public boolean estDisponible(Salle salle, String creneau) {
-      for (Resarvation r : reservations) {
-         if (r.getSalle().getNom().equals(salle.getNom())&& r.getCreneau().equals(creneau)) {
-            return false;
-         }
-      }
-      return true;
-     }
-     
-     public static void main(string[] args){
-        System.out.println();
-     }
+    private List<Reservation> reservations = new ArrayList<>();
+    private List<Salle> salles = new ArrayList<>();
+
+    public boolean estDisponible(Salle salle, String creneau) {
+        for (Reservation r : reservations) {
+            if (r.getSalle().getNom().equals(salle.getNom()) && r.getCreneau().equals(creneau)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean existeSalle(String nom) {
+        for (int i = 0; i < salles.size(); i++) {
+            Salle s = salles.get(i);
+            if (s.getNom().equalsIgnoreCase(nom)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int nombreTotalReservations() {
+        return this.reservations.size();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Outils initialises.");
+    }
 }
